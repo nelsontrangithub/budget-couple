e5030beab226d94c7c966cdd0b8c6d992fe7460f
Nelson Tran <ntran@cadmakers.com>
2020-10-25 13:27:37 -0700

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { ROUTES } from './constants/routes';
+import Home from './components/Home';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path={ROUTES.HOME} component={Home} />
+      {/* <Route path={ROUTES.SIGN_IN} component={SignIn} /> */}
+      <Route component={Home} />
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const Home = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home;
