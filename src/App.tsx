6f54a0001b4cd6a6a974888e14bb50283d6a6173
Nelson Tran <ntran@cadmakers.com>
2020-11-01 15:21:10 -0800

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import { ROUTES } from './constants/routes';
 import Home from './components/Home';
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import styles from '../styles/Home.module.scss';
 
 interface Props {
     
 }
 
+
 export const Home = (props: Props) => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+    </div>
+  );
 }
 
 export default Home;
